b1a4a7aa20116d94a740dd88d45903ab6f88217f
zalazalaza <flightofthepigeon@gmail.com>
2021-12-14 08:14:46 -0500

    remove useless

--- 14/14.py
+++ 14/14.py
@@ -13,19 +13,14 @@ def insertion(filename):
                 if j < len(sequence)-1:
                     scores[sequence[j]+sequence[j+1]] += 1
         for letters in scores:
+            my_dict[letters[0]+data[letters]] += scores[letters]
+            my_dict[data[letters] + letters[1]] += scores[letters]
             if scores[letters] > 0 and data[letters] in letter_score:
                 letter_score[data[letters]] += scores[letters]
             elif scores[letters] > 0 and data[letters] not in letter_score:
                 letter_score[data[letters]] = 1
-        for letters in scores:
-            if scores[letters] > 0:
-                my_dict[letters[0]+data[letters]] += scores[letters]
-                my_dict[data[letters] + letters[1]] += scores[letters]
         scores = my_dict
     return letter_score[max(letter_score, key=letter_score.get)] - letter_score[min(letter_score, key=letter_score.get)]
-
-
-
 
 if __name__ == "__main__":
     print(insertion("14.txt"))
